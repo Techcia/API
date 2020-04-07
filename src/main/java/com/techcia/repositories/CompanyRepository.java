122be2912f2bb45371272f5085e813fca826262f
Leanez <leanez024@gmail.com>
2020-04-07 16:21:42 -0300

    correções

--- src/main/java/com/techcia/repositories/CompanyRepository.java
+++ src/main/java/com/techcia/repositories/CompanyRepository.java
@@ -1,11 +1,10 @@
 package com.techcia.repositories;
 
-import com.techcia.models.Client;
 import com.techcia.models.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface CompanyRepository extends JpaRepository <Company, Long> {
-    Optional<Client> findByEmail(String email);
+    Optional<Company> findById(Long id);
 }
